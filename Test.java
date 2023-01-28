71538ee94ffba0860c3fd762b0a69ef58e943f30
unknown <shalinig.xworkz@gmail.com>
2023-01-27 20:54:22 -0800

    test

---
+++ Test.java
@@ -0,0 +1,36 @@
+1.Explain java and javac
+-java is a object oriented program which is used in the real time application.
+javac is a java compiler which check the program to invoke.
+
+2.Explain JDK,JRE,JVM
+ - JDK-java development kit.
+  JRE-java runtime error.
+  JVM-java virtual machine.
+  
+4.Explain class,object,signature of class
+ -class is a real world entity and it can be seen physically  
+  object is a class file
+  
+5.Difference between .java file and .class file
+  -.java fie is input to the java program.
+   .class file is output of the program.
+
+6.Explain the method signature
+ -access specifier non access specifier return type methode(parameters)throws classname,classname
+ 
+7.What is java
+ - java is a platform independent language.and it is a object oriented prorgam.
+
+9.What is variable? Explain its types
+ -variables are which store the adress of variable or locate the  given data types to its respective location .
+ types are-static variable and non static variable,local variables and global variables.
+ 
+10.list the 15 java keywords
+ -class,object, static,non static,int,void ,main,string,float,double,long,short,char,arrays,public.
+ 
+12.What is data types and Explain its types
+ - data types are the type of data is used to  
+
+
+
+
